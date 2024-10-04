e34e2f5bac3d012f010978c785104c155f49ab2d
lp407 <62835749+lp407@users.noreply.github.com>
2024-10-04 12:35:46 +0100

    include glfw

--- Autofilm/src/Window.h
+++ Autofilm/src/Window.h
@@ -1,10 +1,32 @@
 #pragma once
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <array>
 
-namespace Autofilm{
-    class Window{
+namespace Autofilm
+{
+
+    class Window
+    {
     public:
         Window() {}
         ~Window() {}
 
+        void init(int width, int height);
+        static void callbackResize(GLFWwindow* window, int cx, int cy);
+        void mainLoop();
+        bool isFullscreen(){};
+        void setFullscreen(bool fullscreen);
+
+    private:
+        std::array<int, 2> _wndPos {0, 0};
+        std::array<int, 2> _wndSize {0, 0};
+        std::array<int,  2> _vpSize {0, 0};
+        bool _updateViewport {true};
+        GLFWwindow* _wnd {nullptr};
+        GLFWmonitor* _monitor {nullptr};
+
+        void resize(int cx, int cy);
     };
+    
 }
