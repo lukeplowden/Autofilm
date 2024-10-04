b180b87ba21c4855c90e064d7a6a556f9614bf5b
lp407 <62835749+lp407@users.noreply.github.com>
2024-10-04 13:07:30 +0100

    define some function

--- Autofilm/src/Window.cpp
+++ Autofilm/src/Window.cpp
@@ -2,5 +2,32 @@
 
 namespace Autofilm
 {
+    Window::Window(/* args */)
+    {
+    }
     
+    Window::~Window()
+    {
+
+    }
+
+    void Window::init(int width, int height)
+    {
+
+    }
+    
+    void Window::mainLoop()
+    {
+
+    }
+
+    bool Window::isFullscreen()
+    {
+        return glfwGetWindowMonitor(_wnd) != nullptr;
+    }
+
+    void Window::setFullscreen(bool fullscreen)
+    {
+
+    }
 }
