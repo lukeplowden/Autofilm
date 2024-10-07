59e123ee2120efeef3c4606e9ec14364d2d12f86
lp407 <62835749+lp407@users.noreply.github.com>
2024-10-07 18:55:15 +0100

    implement basic glfw window

--- Autofilm/src/Core/OpenGLWindow.cpp
+++ Autofilm/src/Core/OpenGLWindow.cpp
@@ -1,34 +1,82 @@
 #include "autofilmpch.h"
 #include "Core/OpenGLWindow.h"
+#include "Core/Log.h"
 
 namespace Autofilm
 {
-    OpenGLWindow::OpenGLWindow()
+    static bool s_GLFWInitialised = false;
+
+    Window* Window::Create(const WindowProperties& props)
     {
+        return new OpenGLWindow(props);
     }
-    
+ 
+    OpenGLWindow::OpenGLWindow(const WindowProperties& props)
+    {
+        init(props);
+    }
+
     OpenGLWindow::~OpenGLWindow()
     {
-
+        shutdown();
     }
 
-    void OpenGLWindow::init(int width, int height)
+    void OpenGLWindow::init(const WindowProperties& props)
     {
+        m_data.title = props.title;
+        m_data.width = props.width;
+        m_data.height = props.height;
 
+        AF_CORE_INFO("Creating window {0} ({1}, {2})", props.title, props.width, props.height);
+
+        if (!s_GLFWInitialised)
+        {
+            int success = glfwInit();
+            AF_CORE_ASSERT(success, "Could not initialise GLFW.");
+
+            s_GLFWInitialised = true;
+        }
+        m_window = glfwCreateWindow((int)props.width, (int)props.height, props.title.c_str(), nullptr, nullptr);
+        glfwMakeContextCurrent(m_window);
+        int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+        AF_CORE_ASSERT(status, "Failed to initalise Glad!");
+        glfwSetWindowUserPointer(m_window, &m_data);
+        setVSync(true);
     }
-    
-    void OpenGLWindow::mainLoop()
-    {
 
+    void OpenGLWindow::shutdown()
+    {
+        glfwDestroyWindow(m_window);
     }
 
-    bool OpenGLWindow::isFullscreen()
+    void OpenGLWindow::onUpdate()
     {
-        return glfwGetWindowMonitor(_wnd) != nullptr;
+        glfwPollEvents();
+        glfwSwapBuffers(m_window);
+    }
+
+    void OpenGLWindow::setVSync(bool enabled)
+    {
+        if (enabled){
+            glfwSwapInterval(1);
+        } else {
+            glfwSwapInterval(0);
+        }
+        m_data.VSync = enabled;
+    }
+
+    bool OpenGLWindow::isVSync() const
+    {
+        return m_data.VSync;
     }
 
     void OpenGLWindow::setFullscreen(bool fullscreen)
     {
+        return;
+    }
 
+    bool OpenGLWindow::isFullscreen() const
+    {
+        return m_data.fullscreen;
     }
 }
