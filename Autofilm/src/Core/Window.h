ce462a494296b880fae8675be2e1e84cf3bef7e5
lp407 <62835749+lp407@users.noreply.github.com>
2024-10-07 18:55:53 +0100

    platform independent window interface

---
+++ Autofilm/src/Core/Window.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "autofilmpch.h"
+#include "Core/Core.h"
+
+namespace Autofilm
+{
+    struct WindowProperties
+    {
+        std::string title;
+        unsigned int width;
+        unsigned int height;
+
+        WindowProperties(const std::string& title = "Autofilm", 
+                        unsigned int width = 1280,
+                        unsigned int height = 720)
+            : title(title), width(width), height(height)
+        {}
+    };
+
+    class AUTOFILM_API Window
+    {
+    public:
+        // using eventCallbackFn = std::function<void(Event&)>;
+
+        virtual ~Window() {}
+
+        virtual void onUpdate() = 0;
+
+        virtual unsigned int getWidth() const = 0;
+        virtual unsigned int getHeight() const = 0;
+    
+        // Window attributes
+        // virtual void setEventCallback(const eventCallbackFn& callback) = 0;
+        virtual void setVSync(bool enabled) = 0;
+        virtual bool isVSync() const = 0;
+        virtual void setFullscreen(bool fullscreen) = 0;
+        virtual bool isFullscreen() const = 0;
+
+        static Window* Create(const WindowProperties&  props = WindowProperties());
+    };
+}
