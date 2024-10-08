5eee16a27fd4976c539e1ee4f45a0889e6bb539f
lp407 <62835749+lp407@users.noreply.github.com>
2024-10-08 17:51:54 +0100

    currently broken renderer init

--- Autofilm/src/Core/EntryPoint.h
+++ Autofilm/src/Core/EntryPoint.h
@@ -10,7 +10,7 @@ int	main(int argc, char** argv)
 	Autofilm::Log::Init();
 	AF_CORE_WARN("Initialised Log!");
 	AF_INFO("Initialised Log!");
-
+	Autofilm::Renderer::init(Autofilm::RenderAPIType::OpenGL);
 	auto app = Autofilm::CreateApp();
 	app->Run();
 	delete app;
