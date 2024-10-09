b1c4e4dc382e7cc0a457746ab523a0299d66a6fc
lp407 <62835749+lp407@users.noreply.github.com>
2024-10-09 10:21:48 +0100

    temporarily playing with multi window before refactor

--- Autofilm/src/Core/App.cpp
+++ Autofilm/src/Core/App.cpp
@@ -1,6 +1,9 @@
 #include "autofilmpch.h"
 #include "App.h"
 #include "Log.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "OpenGL/OpenGLWindow.h"
 
 namespace Autofilm
 {
@@ -18,12 +21,19 @@ namespace Autofilm
     {
         m_windows.push_back(std::unique_ptr<Window>(Window::Create()));
         m_windows.push_back(std::unique_ptr<Window>(Window::Create()));
+        m_windows.push_back(std::unique_ptr<Window>(Window::Create()));
         while (m_running)
         {
+            float i = 0.0f;
             for (auto& window : m_windows)
             {
+                OpenGLWindow* openglWindow = dynamic_cast<OpenGLWindow*>(window.get()); 
+                glfwMakeContextCurrent(openglWindow->getWindow());
                 window->onUpdate();
+                glClearColor(i, 0.0f, 0.5f, 1.0f);
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                i += 0.1f;
             }
         }
     }
-}
+}  
