687705fe80eb82039686860781e70f94b59d99be
23036879 <l.plowden0620231@arts.ac.uk>
2024-10-14 17:47:38 +0100

    hello gstreamer tutorial

---
+++ Autofilm/src/Video/VideoPlayer.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <gst/gst.h>
+
+int AUTOFILM_API hello_gst()
+{
+    GstElement* pipeline;
+    GstBus* bus;
+    GstMessage *msg;
+    gst_init(NULL, NULL);
+    
+    pipeline = gst_parse_launch(
+        "playbin uri=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm",
+        NULL
+    );
+
+    /* Start playing */
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+    // /* Wait until error or EOS */
+    bus = gst_element_get_bus(pipeline);
+    msg = gst_bus_timed_pop_filtered(
+        bus, 
+        GST_CLOCK_TIME_NONE,
+        (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS)
+    );
+
+    // /* See next tutorial for proper error message handling/parsing */
+    if (msg != NULL) {
+        if (GST_MESSAGE_TYPE(msg) == GST_MESSAGE_ERROR) {
+            g_printerr("An error occurred! Re-run with the GST_DEBUG=*:WARN "
+                       "environment variable set for more details.\n");
+        }
+        gst_message_unref(msg);
+    }
+
+    // /* Free resources */
+    gst_object_unref (bus);
+    gst_element_set_state (pipeline, GST_STATE_NULL);
+    gst_object_unref (pipeline);
+    return 0;
+}
