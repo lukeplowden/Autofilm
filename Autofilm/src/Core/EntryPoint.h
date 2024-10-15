feca268b95bb5542e2c7616d094a89d939e0d426
23036879 <l.plowden0620231@arts.ac.uk>
2024-10-15 17:00:32 +0100

    move renderer init back into main

--- Autofilm/src/Core/EntryPoint.h
+++ Autofilm/src/Core/EntryPoint.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #ifdef AF_PLATFORM_WINDOWS
+#include "Core.h"
 #include "App.h"
 
 extern Autofilm::App* Autofilm::CreateApp();
@@ -8,10 +9,11 @@ extern Autofilm::App* Autofilm::CreateApp();
 int	main(int argc, char** argv)
 {
 	Autofilm::Log::Init();
-	AF_CORE_WARN("Initialised Log!");
+
+	AF_CORE_INFO("Initialised Log!");
 	AF_INFO("Initialised Log!");
-	// Autofilm::Renderer::init(Autofilm::RenderAPIType::OpenGL);
 	auto app = Autofilm::CreateApp();
+    Autofilm::Renderer::init(app->_settings.renderer);
 	app->Run();
 	delete app;
 }
