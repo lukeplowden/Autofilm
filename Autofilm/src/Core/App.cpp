e8190ff3e36905a33e180abfa0b05910d5c6b593
23036879 <l.plowden0620231@arts.ac.uk>
2024-10-15 17:00:02 +0100

    remove gl code

--- Autofilm/src/Core/App.cpp
+++ Autofilm/src/Core/App.cpp
@@ -15,26 +15,17 @@ namespace Autofilm
 
     App::~App()
     {
-
+        
     }
 
     void App::Run()
     {
-        m_windows.push_back(std::unique_ptr<Window>(Window::Create()));
-        // m_windows.push_back(std::unique_ptr<Window>(Window::Create()));
-        // m_windows.push_back(std::unique_ptr<Window>(Window::Create()));
         hello_gst();
         while (m_running)
         {
-            float i = 0.0f;
-            for (auto& window : m_windows)
+            for (auto& window : _windows)
             {
-                OpenGLWindow* openglWindow = dynamic_cast<OpenGLWindow*>(window.get()); 
-                glfwMakeContextCurrent(openglWindow->getWindow());
                 window->onUpdate();
-                glClearColor(i, 0.0f, 0.5f, 1.0f);
-                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-                i += 0.1f;
             }
         }
     }
