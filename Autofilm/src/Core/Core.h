f69da015fb0a8194a8ff2257031ac3ff604c2018
23036879 <l.plowden0620231@arts.ac.uk>
2024-10-15 17:57:52 +0100

    added not equal assert as vulkan sometimes needs

--- Autofilm/src/Core/Core.h
+++ Autofilm/src/Core/Core.h
@@ -16,6 +16,7 @@
 	#define AF_CORE_ASSERT_EQUAL(x, y, ...) { if(x != y) { AF_CORE_ERROR("Assertion failed {0}", __VA_ARGS__); __debugbreak(); } }
 	#define AF_VK_ASSERT(x, ...) { if(!(x)) { AF_VK_ERROR("Assertion failed {0}", __VA_ARGS__); __debugbreak(); } }
 	#define AF_VK_ASSERT_EQUAL(x, y, ...) { if(x != y) { AF_VK_ERROR("Assertion failed {0}", __VA_ARGS__); __debugbreak(); } }
+	#define AF_VK_ASSERT_NOT_EQUAL(x, y, ...) { if(x == y) { AF_VK_ERROR("Assertion failed {0}", __VA_ARGS__); __debugbreak(); } }
 #else
 	#define AF_ASSERT(x, ...)
 	#define AF_CORE_ASSERT(x, ...)
