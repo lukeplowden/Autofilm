31012912755e4f6ee26c253ef2827b38b5305b07
23036879 <l.plowden0620231@arts.ac.uk>
2024-10-16 16:10:06 +0100

    event system cherno impl

--- Autofilm/src/Events/Event.h
+++ Autofilm/src/Events/Event.h
@@ -1,21 +1,63 @@
 #pragma once
 
 #include "Core/Core.h"
-#include <variant>
+#include <string>
+#include <functional>
 
 namespace Autofilm
 {
+    enum class EventType
+    {
+        None = 0,
+        WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+        AppTick, AppUpdate, AppRender,
+        KeyPressed, KeyReleased,
+        MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+    };
+
+    enum EventCategory
+    {
+      None = 0,
+      EventCategoryApplication  = BIT(0),  
+      EventCategoryInput        = BIT(1),  
+      EventCategoryKeyboard     = BIT(2),  
+      EventCategoryMouse        = BIT(3),  
+      EventCategoryMouseButton  = BIT(4),  
+    };
+
     class Event
     {
+        friend class EventDispatcher;
     public:
         enum struct Type 
         { 
             Keyboard, Mouse, App 
         };
 
-        Type getType() const { return type; }
+    protected:
+        bool _handled;
+    };
 
+    class EventDispatcher
+    {
+        template<typename T>
+        using EventFn = std::function<bool(T&)>;
+    public:
+        EventDispatcher(Event& event)
+            : _event(event)
+        {
+        }
+
+        template <typename T>
+        bool dispatch(EventFn<T> func)
+        {
+            if(_event.getEventType() == T::getStaticType()){
+                _event.m_Handled = func(*(T*)&m_event);
+                return true;
+            }
+            return false;
+        }
     private:
-        Type type;
+        Event& _event;
     };
 }
