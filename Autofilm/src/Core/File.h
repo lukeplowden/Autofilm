9d395aaff79fd66e2538297dd97ad165dbdfd2f1
23036879 <l.plowden0620231@arts.ac.uk>
2024-10-17 18:09:39 +0100

    preempting file abstraction ?

--- Autofilm/src/Core/File.h
+++ Autofilm/src/Core/File.h
@@ -1,3 +1,14 @@
 #pragma once
 
 #include "Core/Core.h"
+#include <filesystem>
+#include <fstream>
+
+namespace Autofilm
+{
+    class File
+    {
+        static bool exists();
+        
+    };
+}
