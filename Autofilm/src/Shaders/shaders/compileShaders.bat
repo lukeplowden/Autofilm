076a4fc582d4f6291a5535d9b0163308ddff25f1
23036879 <l.plowden0620231@arts.ac.uk>
2024-10-21 16:07:47 +0100

    make shaders bin dir if not exists

--- Autofilm/src/Shaders/shaders/compileShaders.bat
+++ Autofilm/src/Shaders/shaders/compileShaders.bat
@@ -1,3 +1,8 @@
+REM Check if the bin directory exists, if not, create it
+if not exist "bin" (
+    mkdir "bin"
+)
+
 %VULKAN_SDK%/Bin/glslc.exe triangle.vert -o bin/vert.spv
 %VULKAN_SDK%/Bin/glslc.exe triangle.frag -o bin/frag.spv
 pause
